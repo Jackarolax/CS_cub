7c79e973bf25dac1f9eb33c27ed6c014851adce7
Jackarolax <alexnem@outlook.de>
2026-08-02 15:07:23 +0200

    added correct mlx includes

--- cub.h
+++ cub.h
@@ -6,7 +6,16 @@
 /*   By: anematol <anematol@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/02 13:01:50 by anematol          #+#    #+#             */
-/*   Updated: 2026/08/02 13:01:53 by anematol         ###   ########.fr       */
+/*   Updated: 2026/08/02 15:06:34 by anematol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CUB_H
+# define CUB_H
+# include "./minilibx-linux/mlx.h"
+# include <X11/X.h>
+# include <X11/keysym.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+#endif
